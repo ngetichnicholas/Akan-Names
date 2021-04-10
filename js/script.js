02c286d5c4a8eecbaeba9528ddd436d2efc4660f
ngetichnick <nicholas.ngetich@student.moringaschool.com>
2021-04-10 11:27:35 +0300

    Updating javascript

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
-function akanFunction() {
-  let birthDate = document.getElementById("myDate").value;
-let gender = document.getElementById("gender").value;
-var birthDateArray = birthDate.split('-');
-//validation
-if(birthDateArray.length !== 3){
-     alert("invalid Date");
-}else{
-    if(!birthDateArray[0].match(/^\d\d\d\d$/) || 
-       !birthDateArray[1].match(/^\d\d$/) || 
-       !birthDateArray[2].match(/^\d\d$/)){
-        alert("invalid Date");    
+function callingFunction() {
+  function akanNaming() {
+    var bday;
+    bday = document.getElementById("myDate").value;
+    var bdayArray = bday.split('-');
+    
+    
+    //validation
+    if(bdayArray.length !== 3){
+         alert("invalid Date");
     }else{
-        let weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'];
-        let maleNames =['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-        let femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
-        var currentTime = new Date(            
-            parseInt(bdayArray[0]),
-            parseInt(bdayArray[1]) - 1, //month starts from 0
-            parseInt(bdayArray[2])
-        );        
-        var currentDay = currentTime.getDay();       
-        var currentDayName= days[currentDay];
-       document.write("You were born on "+currentDayName);
+        if(!bdayArray[0].match(/^\d\d\d\d$/) || 
+           !bdayArray[1].match(/^\d\d$/) || 
+           !bdayArray[2].match(/^\d\d$/)){
+            alert("invalid Date");    
+        }else{
+            var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'];     
+            var currentTime = new Date(            
+                parseInt(bdayArray[0]),
+                parseInt(bdayArray[1]) - 1, //month starts from 0
+                parseInt(bdayArray[2])
+            );        
+            var currentDay = currentTime.getDay();       
+            var currentDayName= days[currentDay];
+            let output = "You were born on "+currentDayName;
+            return output;
+        }
     }
-}
+    }
+    document.getElementById("hidden").style.display = 'block';
+    document.getElementById("output").innerHTML = akanNaming();
 }
